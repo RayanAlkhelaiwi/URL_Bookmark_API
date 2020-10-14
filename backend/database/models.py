9da36fab8368ac40e62581e0f8410b84ba8bad4a
RayanAlkhelaiwi <r.n.m.k@live.com>
2020-10-14 20:57:55 +0300

    Change DB models logic to URL Bookmark
    Instead of URL Shortener

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,10 +1,9 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, Boolean, create_engine, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "url_shortener"
+database_name = "bookmark"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
@@ -23,20 +22,23 @@ def setup_db(app, database_path=database_path):
     db.create_all()
 
 
-class long_url(db.Model):
+# Database Models
+
+class Bookmark(db.Model):
     '''
-        Long/Original URL to be shorten
+        Table to bookmark URLs
     '''
-    __tablename__ = 'long_url'
+    __tablename__ = 'bookmark'
 
     id = Column(Integer, primary_key=True)
+    title = Column(String)
     url = Column(String)
-    short_url_id = Column(Integer, ForeignKey('short_url.id'))
-    short_url = relationship('short_url', foreign_keys=short_url_id)
+    # category_id = Column(Integer, ForeignKey('Category.id'))
+    # category = relationship('category', foreign_keys=category_id)
 
-    def __init__(self, url, short_url):
+    def __init__(self, title, url):
+        self.title = title
         self.url = url
-        self.short_url = short_url
 
     def insert(self):
         db.session.add(self)
@@ -52,25 +54,28 @@ class long_url(db.Model):
     def format(self):
         return {
             'id': self.id,
-            'url': self.url,
-            'short_url': self.short_url
+            'title': self.title,
+            'url': self.url
         }
 
 
-class short_url(db.Model):
+class Category(db.Model):
     '''
-        Shorten URL of the long/original URL (To be directed tos)
+        Table to categorise bookmarked and determine its importance to read
     '''
-    __tablename__ = 'short_url'
+    __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
-    url = Column(String)
+    category_type = Column(String)
+    is_important = Column(Boolean)
 
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, category_type, is_important):
+        self.category_type = category_type
+        self.is_important = is_important
 
     def format(self):
         return {
             'id': self.id,
-            'short_url': self.url
+            'type': self.category_type,
+            'important': self.is_important
         }
