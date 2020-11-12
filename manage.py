566eea52c65fca6916242f923a6b4c6ee620dd27
RayanAlkhelaiwi <r.n.m.k@live.com>
2020-11-12 23:48:47 +0300

    Update database migration manage file
    For heroku deployment

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import create_app as app
+from app import app
 from models import db
 
 migrate = Migrate(app, db)
