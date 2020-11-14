2feaf0c8f4d4bdaa5d839c33840c28015973c819
RayanAlkhelaiwi <r.n.m.k@live.com>
2020-11-14 22:45:25 +0300

    Update db model file to follow PEP8 Formatting

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 import os
-from sqlalchemy import Column, String, Integer, Boolean, create_engine, ForeignKey
+from sqlalchemy import (Column, String, Integer, 
+                        Boolean, create_engine, ForeignKey)
 from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "bookmark"
-database_path = os.environ['DATABASE_URL'] # "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
+# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
 
 db = SQLAlchemy()
-
 
 def setup_db(app, database_path=database_path):
     '''
